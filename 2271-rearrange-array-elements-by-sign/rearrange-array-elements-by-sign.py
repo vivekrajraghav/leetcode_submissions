eabd8a044cb4dd9709c923d50b53a1c69321ac0c
Vivek <vivekrajraghav@gmail.com>
2026-07-28 20:37:09 +0530

    Time: 58 ms (33.10%) | Memory: 42.9 MB (73.59%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,14 +1,29 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        positive_arr=[]
-        negative_arr=[]
-        for i in range(0,len(nums)):
-            if nums[i]>0:
-                positive_arr.append(nums[i])
-            if nums[i]<0:
-                negative_arr.append(nums[i])
-        new_list=[]
-        for i in range(0,len(positive_arr)):
-            new_list.append(positive_arr[i])
-            new_list.append(negative_arr[i])
-        return new_list
+    #   Brute Force Approach
+        # positive_arr=[]
+        # negative_arr=[]
+        # for i in range(0,len(nums)):
+        #     if nums[i]>0:
+        #         positive_arr.append(nums[i])
+        #     if nums[i]<0:
+        #         negative_arr.append(nums[i])
+        # new_list=[]
+        # for i in range(0,len(positive_arr)):
+        #     new_list.append(positive_arr[i])
+        #     new_list.append(negative_arr[i])
+        # return new_list
+
+        # Better approach
+        n=len(nums)
+        result=[0]*n
+        i=0
+        j=1
+        for k in range(0,n):
+            if nums[k]>0:
+                result[i]=nums[k]
+                i+=2
+            if nums[k]<0:
+                result[j]=nums[k]
+                j+=2
+        return result
