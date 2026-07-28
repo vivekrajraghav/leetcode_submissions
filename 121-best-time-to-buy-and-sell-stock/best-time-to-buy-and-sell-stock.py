585a267b0f897c1a49c2a0169244c3f5663d1b8b
Vivek <vivekrajraghav@gmail.com>
2026-07-28 20:02:14 +0530

    Time: 63 ms (25.95%) | Memory: 28.7 MB (19.93%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,13 @@ class Solution:
         min_prices=float("inf")
         max_profit=0
         for i in range(0,n):
+            # if min_prices>prices[i]:
+            #     min_prices=prices[i]
+            # if min_prices<prices[i]:
+            #     profit=prices[i]-min_prices
+            #     if profit>max_profit:
+            #         max_profit=profit
             min_prices=min(min_prices,prices[i])
-            if min_prices<prices[i]:
-                profit=prices[i]-min_prices
-                if profit>max_profit:
-                    max_profit=profit
+            max_profit=max(max_profit,prices[i]-min_prices)
         return max_profit
 
