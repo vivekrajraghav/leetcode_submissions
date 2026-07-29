2a57bac83621aef505165b2dae7ec09cbd453484
Vivek <vivekrajraghav@gmail.com>
2026-07-29 10:15:49 +0530

    Time: 5 ms (69.06%) | Memory: 20.9 MB (11.70%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,23 +3,40 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        def mark_infinity(matrix,i_index,j_index):
-            r=len(matrix)
-            c=len(matrix[0])
-            for i in range(0,r):
-                if matrix[i][j_index]!=0:
-                    matrix[i][j_index]=float("inf")
-            for j in range(0,c):
-                if matrix[i_index][j]!=0:
-                    matrix[i_index][j]=float("inf")
+        # def mark_infinity(matrix,i_index,j_index):
+        #     r=len(matrix)
+        #     c=len(matrix[0])
+        #     for i in range(0,r):
+        #         if matrix[i][j_index]!=0:
+        #             matrix[i][j_index]=float("inf")
+        #     for j in range(0,c):
+        #         if matrix[i_index][j]!=0:
+        #             matrix[i_index][j]=float("inf")
+        # row=len(matrix)
+        # col=len(matrix[0])
+        # for i in range(0,row):
+        #     for j in range(0,col):
+        #         if matrix[i][j]==0:
+        #             mark_infinity(matrix,i,j)
+        # for i in range(0,row):
+        #     for j in range(0,col):
+        #         if matrix[i][j]==float("inf"):
+        #             matrix[i][j]=0
+
+        # Optimal Solution
         row=len(matrix)
         col=len(matrix[0])
+        row_tracker=[0]*row
+        col_tracker=[0]*col
         for i in range(0,row):
             for j in range(0,col):
                 if matrix[i][j]==0:
-                    mark_infinity(matrix,i,j)
+                    row_tracker[i]=-1
+                    col_tracker[j]=-1
         for i in range(0,row):
             for j in range(0,col):
-                if matrix[i][j]==float("inf"):
-                    matrix[i][j]=0
+                if row_tracker[i]==-1 or col_tracker[j]==-1:
+                    matrix[i][j]=0 
+
+
 
