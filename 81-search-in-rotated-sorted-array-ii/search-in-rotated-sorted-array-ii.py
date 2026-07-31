0205a7f25a315d1a9a9ff0803354399015648c13
Vivek <vivekrajraghav@gmail.com>
2026-07-31 09:18:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (68.38%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,16 +1,41 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
-        nums.sort()
+        # nums.sort()
+        # n=len(nums)
+        # low=0
+        # high=n-1
+        # while high>=low:
+        #     mid=(low+high)//2
+        #     mid_value=nums[mid]
+        #     if mid_value==target:
+        #         return True
+        #     elif mid_value>target:
+        #         high=mid-1
+        #     elif mid_value<target:
+        #         low=mid+1
+        # return False
+
+        # Without .sort()
         n=len(nums)
         low=0
         high=n-1
         while high>=low:
             mid=(low+high)//2
-            mid_value=nums[mid]
-            if mid_value==target:
+            if nums[mid]==target:
                 return True
-            elif mid_value>target:
-                high=mid-1
-            elif mid_value<target:
-                low=mid+1
+            if nums[mid]==nums[low]==nums[high]:
+                low+=1
+                high-=1
+                continue
+            if nums[low]<=nums[mid]: # If left part is stoted
+                if nums[low]<=target<nums[mid]:
+                    high=mid-1
+                else:
+                    low=mid+1
+            else: # IF right part is sorted
+                if nums[mid]<target<=nums[high]:
+                    low=mid+1
+                else:
+                    high=mid-1
         return False
+            
