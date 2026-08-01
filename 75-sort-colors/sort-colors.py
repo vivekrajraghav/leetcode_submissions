f7f244ab315a8d67d7fea01c5bbaff18d2ef067d
Vivek <vivekrajraghav@gmail.com>
2026-08-01 16:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (90.24%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,8 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        #Using Bubble Sort
+        # n=len(nums)
+        # for i in range(n-2,-1,-1):
+        #    for j in range(0,i+1):
+        #     if nums[j]>nums[j+1]:
+        #         nums[j],nums[j+1]=nums[j+1],nums[j]
+        
+        # Dutch National Flag Algo
         n=len(nums)
-        for i in range(n-2,-1,-1):
-           for j in range(0,i+1):
-            if nums[j]>nums[j+1]:
-                nums[j],nums[j+1]=nums[j+1],nums[j]
+        start=0
+        mid=0
+        end=n-1
+        while end>=mid:
+            if nums[mid]==0:
+                nums[mid],nums[start]=nums[start],nums[mid]
+                start+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[end]=nums[end],nums[mid]
+                end-=1
