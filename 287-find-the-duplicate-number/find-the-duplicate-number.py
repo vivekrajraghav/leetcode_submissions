cfee1bfd1e5134f2180950b77a47e0dca798f417
Vivek <vivekrajraghav@gmail.com>
2026-08-07 16:18:00 +0530

    Time: 23 ms (77.33%) | Memory: 33.5 MB (74.11%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,5 +1,18 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # Using Floyd Hare and Tortoise Algo
+        slow=0
+        fast=0
+        while True:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+            if slow==fast:
+                break
+        slow=0
+        while slow!=fast:
+            slow=nums[slow]
+            fast=nums[fast]
+        return slow
         #Using XOR
         # n=len(nums)
         # nums.sort()
@@ -8,19 +21,19 @@ class Solution:
         #         return nums[i]
         
         # Using Binary Search
-        n=len(nums)
-        low=0
-        high=n-1
-        while high>low:
-            mid=(low+high)//2
-            count=0
-            for num in nums:
-                if num<=mid:
-                    count+=1
-            if count>mid:
-                high=mid
-            else:
-                low=mid+1
-        return low
+        # n=len(nums)
+        # low=0
+        # high=n-1
+        # while high>low:
+        #     mid=(low+high)//2
+        #     count=0
+        #     for num in nums:
+        #         if num<=mid:
+        #             count+=1
+        #     if count>mid:
+        #         high=mid
+        #     else:
+        #         low=mid+1
+        # return low
                 
             
