c68e956cf9dd3db8924bd920b4ed7568569b96bd
Vivek <vivekrajraghav@gmail.com>
2026-08-14 15:53:44 +0530

    Time: 1 ms (28.07%) | Memory: 19.1 MB (86.90%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.py
@@ -1,6 +1,9 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
-        push=n.bit_length()
-        largest=2**31-1
-        new_num=largest>>(31-push)
-        return new_num
+        # push=n.bit_length()
+        # largest=2**31-1
+        # new_num=largest>>(31-push)
+        # return new_num
+
+        # More consize and better
+        return (1<<n.bit_length())-1
