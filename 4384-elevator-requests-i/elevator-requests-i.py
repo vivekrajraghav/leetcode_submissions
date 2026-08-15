0272b355aa430168e8c9f8d15b00929e255ddbca
Vivek <vivekrajraghav@gmail.com>
2026-08-15 22:37:21 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.37%) - LeetSync

---
+++ 4384-elevator-requests-i/elevator-requests-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def elevatorRequests(self, n: int, requests: list[int]) -> int:
+        total = 0
+        prev = 0
+        for req in requests:
+            total += abs(req - prev)
+            prev = req
+            
+        return total
