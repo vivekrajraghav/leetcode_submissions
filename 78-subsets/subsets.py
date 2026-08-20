24de34f31cd60c37bd92d43438840ab736a2d7fa
Vivek <vivekrajraghav@gmail.com>
2026-08-20 11:13:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.00%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,12 +1,25 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        total_subset=1<<n
+        # n=len(nums)
+        # total_subset=1<<n
+        # result=[]
+        # for num in range(total_subset):
+        #     lst=[]
+        #     for i in range(n):
+        #         if num&(1<<i)!=0:
+        #             lst.append(nums[i])
+        #     result.append(lst)
+        # return result
+
+        # Using Recursion
         result=[]
-        for num in range(total_subset):
-            lst=[]
-            for i in range(n):
-                if num&(1<<i)!=0:
-                    lst.append(nums[i])
-            result.append(lst)
+        def sub_sets(idx,subset):
+            if idx>=len(nums):
+                result.append(subset.copy())
+                return
+            subset.append(nums[idx])
+            sub_sets(idx+1,subset)
+            subset.pop()
+            sub_sets(idx+1,subset)
+        sub_sets(0,[])
         return result
