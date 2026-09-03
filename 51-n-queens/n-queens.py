e68df129f818330717637edd7ddb58c9a692180b
Vivek <vivekrajraghav@gmail.com>
2026-09-03 23:14:49 +0530

    Time: 11 ms (66.45%) | Memory: 19.4 MB (99.76%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,28 +1,24 @@
 class Solution:
-    def solve(self,col,board,ans,leftrow,lowerDiagonal,upperDiagonal,n):
+    def solve(self,col,board,ans,leftrow,lowerdiagonal,upperdiagonal,n):
         if col==n:
             ans.append(board[:])
             return
         for row in range(n):
-            if (
-                leftrow[row]==0
-                and upperDiagonal[n-1+row-col]==0
-                and lowerDiagonal[row+col]==0
-            ):
-                board[row]=board[row][:col]+"Q" +board[row][col+1:]
-                lowerDiagonal[row+col]=1
-                upperDiagonal[n-1+row-col]=1
+            if (leftrow[row]==0 and lowerdiagonal[row+col]==0 and upperdiagonal[n-1+row-col]==0):
+                board[row]=board[row][:col]+"Q"+board[row][col+1:]
                 leftrow[row]=1
-                self.solve(col+1,board,ans,leftrow,lowerDiagonal,upperDiagonal,n)
+                lowerdiagonal[row+col]=1
+                upperdiagonal[n-1+row-col]=1
+                self.solve(col+1,board,ans,leftrow,lowerdiagonal,upperdiagonal,n)
                 board[row]=board[row][:col]+"."+board[row][col+1:]
-                lowerDiagonal[row+col]=0
-                upperDiagonal[n-1+row-col]=0
                 leftrow[row]=0
+                lowerdiagonal[row+col]=0
+                upperdiagonal[n-1+row-col]=0
     def solveNQueens(self, n: int) -> List[List[str]]:
+        leftrow=[0]*n
+        lowerdiagonal=[0]*(2*n-1)
+        upperdiagonal=[0]*(2*n-1)
         ans=[]
         board=["."*n for _ in range(n)]
-        leftrow=[0]*n
-        upperDiagonal=[0]*(2*n-1)
-        lowerDiagonal=[0]*(2*n-1)
-        self.solve(0,board,ans,leftrow,lowerDiagonal,upperDiagonal,n)
+        self.solve(0,board,ans,leftrow,lowerdiagonal,upperdiagonal,n)
         return ans
