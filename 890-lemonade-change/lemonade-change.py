93cce5f6421d547d79bf7c8a9f68f442352a10d7
Vivek <vivekrajraghav@gmail.com>
2026-09-22 14:53:06 +0530

    Time: 10 ms (11.63%) | Memory: 23.4 MB (76.92%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lemonadeChange(self, bills: list[int]) -> bool:
+        my_dict={"5":0,"10":0,"20":0}
+        n=len(bills)
+        for i in range(n):
+            if bills[i]==5:
+                my_dict["5"]+=1
+            elif bills[i]==10:
+                if my_dict["5"]>0:
+                    my_dict["10"]+=1
+                    my_dict["5"]-=1
+                else:
+                    return False
+            else:
+                if my_dict["5"]>=1 and my_dict["10"]>=1:
+                    my_dict["5"]-=1
+                    my_dict["10"]-=1
+                elif my_dict["5"]>=3:
+                    my_dict["5"]-=3
+                else:
+                    return False
+        return True
